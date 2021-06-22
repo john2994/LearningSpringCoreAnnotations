d3d4d8113ab3b95be7c5e126998bc24c37cbf63c
karibov@btlab.ru <12345678>
2021-06-22 07:39:17 +0300

    Practicing Qualifier annotation

--- src/com/spring/demo/RandomFortuneService.java
+++ src/com/spring/demo/RandomFortuneService.java
@@ -1,13 +1,22 @@
 package com.spring.demo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
-
+	//Create an array of strings
+	String[] data = {
+			"Beware of wolfs!",
+			"Second fortune",
+			"Another fortune"
+	};
 	@Override
 	public String getDailyFortune() {
-		return null;
+		//Pick random string from array
+		Random dice = new Random();
+		return data[dice.nextInt(data.length)];
 	}
 
 }
